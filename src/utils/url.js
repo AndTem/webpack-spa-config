4bbe87e6647292bc7cdb1fdfcb267a2b264cfe54
Andrey Potyomkin <ipad_nw@icloud.com>
2019-06-27 10:58:09 +0300

    Поправлено формирование имени файла для изображений в url-loader

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,9 +1,7 @@
-const { join } = require('path');
-
 const { isProduction } = require('./mode');
 
 const urlLoaderFileName = (mode, outputDirectoryName) =>
-  join(outputDirectoryName, `[name]${isProduction(mode) ? '.[hash]' : ''}.[ext]`);
+  `${outputDirectoryName}/[name]${isProduction(mode) ? '.[hash]' : ''}.[ext]`;
 
 module.exports = {
   urlLoaderFileName
